37c9d3e4d9c3cbd111e4e5691cdc4b81715afe6e
jatingaur18 <jatin22101@iiitnr.edu.in>
2025-06-05 16:02:41 +0530

    fix

--- server/scraper.js
+++ server/scraper.js
@@ -8,18 +8,19 @@ async function scrapeEventbriteEvents() {
   try {
     console.log('Starting to scrape Eventbrite events...');
 
-    browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--single-process',
-        '--disable-gpu'
-      ]
+    const browser = await puppeteer.launch({
+        headless: 'new',
+        executablePath: '/usr/bin/google-chrome',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     });
 
     const page = await browser.newPage();
